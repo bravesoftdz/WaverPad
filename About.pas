562b3e15c97b61692653116da82b837ff9424580
Michael Agarkov <michaelagarkov@outlook.com>
2021-10-27 04:20:07 +0300

    Add files via upload

---
+++ About.pas
@@ -0,0 +1,34 @@
+﻿Unit About;
+
+interface
+
+uses System, System.Drawing, System.Windows.Forms;
+
+type
+  AboutForm = class(Form)
+    procedure linkLabel1_LinkClicked(sender: Object; e: LinkLabelLinkClickedEventArgs);
+  {$region FormDesigner}
+  private
+    {$resource About.AboutForm.resources}
+    linkLabel1: LinkLabel;
+    tabControl1: TabControl;
+    tabPage1: TabPage;
+    richTextBox1: RichTextBox;
+    pictureBox1: PictureBox;
+    {$include About.AboutForm.inc}
+  {$endregion FormDesigner}
+  public
+    constructor;
+    begin
+      InitializeComponent;
+    end;
+  end;
+
+implementation
+
+procedure AboutForm.linkLabel1_LinkClicked(sender: Object; e: LinkLabelLinkClickedEventArgs);
+begin
+  exec('https://github.com/MichaelAgarkov');
+end;
+
+end.
---
+++ Unit1.pas
@@ -0,0 +1,128 @@
+﻿unit Unit1;
+
+interface
+
+uses System, System.Drawing, System.Windows.Forms, System.ComponentModel, About;
+
+{$reference 'PresentationCore.dll'}
+
+type
+  Form1 = class(Form)
+    procedure Form1_Load(sender: Object; e: EventArgs);
+    procedure aboutToolStripMenuItem_Click(sender: Object; e: EventArgs);
+    procedure button3_Click(sender: Object; e: EventArgs);
+    procedure listBox1_SelectedIndexChanged(sender: Object; e: EventArgs);
+    procedure button1_Click(sender: Object; e: EventArgs);
+    procedure openFileDialog1_FileOk(sender: Object; e: CancelEventArgs);
+    procedure button2_Click(sender: Object; e: EventArgs);
+    procedure playSelectedSoundToolStripMenuItem_Click(sender: Object; e: EventArgs);
+    procedure addAFileToolStripMenuItem_Click(sender: Object; e: EventArgs);
+    procedure removeToolStripMenuItem_Click(sender: Object; e: EventArgs);
+  {$region FormDesigner}
+  private
+    {$resource Unit1.Form1.resources}
+    soundboardToolStripMenuItem: ToolStripMenuItem;
+    aboutToolStripMenuItem: ToolStripMenuItem;
+    listBox1: ListBox;
+    groupBox1: GroupBox;
+    button2: Button;
+    button1: Button;
+    button3: Button;
+    components: System.ComponentModel.IContainer;
+    label1: &Label;
+    maskedTextBox1: MaskedTextBox;
+    label2: &Label;
+    openFileDialog1: OpenFileDialog;
+    playSelectedSoundToolStripMenuItem: ToolStripMenuItem;
+    toolStripSeparator1: ToolStripSeparator;
+    addAFileToolStripMenuItem: ToolStripMenuItem;
+    removeToolStripMenuItem: ToolStripMenuItem;
+    groupBox2: GroupBox;
+    menuStrip1: MenuStrip;
+    {$include Unit1.Form1.inc}
+  {$endregion FormDesigner}
+  public 
+    constructor;
+    begin
+      InitializeComponent;
+    end;
+  end;
+
+implementation
+
+procedure Form1.Form1_Load(sender: Object; e: EventArgs);
+begin
+  
+end;
+
+procedure Form1.aboutToolStripMenuItem_Click(sender: Object; e: EventArgs);
+begin
+  Form(new AboutForm).show;
+end;
+
+procedure Form1.button3_Click(sender: Object; e: EventArgs);
+begin
+  var SoundboardPlayer := new System.Windows.Media.MediaPlayer;
+  SoundboardPlayer.Open(new System.Uri(listBox1.SelectedItem.ToString, System.UriKind.Relative));
+  SoundboardPlayer.Play();
+  sleep((maskedTextBox1.Text + '000').ToInteger);
+  SoundboardPlayer.Close();
+end;
+
+procedure Form1.listBox1_SelectedIndexChanged(sender: Object; e: EventArgs);
+begin
+  if(listBox1.SelectedItem <> nil) then begin
+    button3.Enabled := true;
+    playSelectedSoundToolStripMenuItem.Enabled := true;
+    button2.Enabled := true;
+    removeToolStripMenuItem.Enabled := true;
+  end;
+end;
+
+procedure Form1.button1_Click(sender: Object; e: EventArgs);
+begin
+  openFileDialog1.ShowDialog;
+end;
+
+procedure Form1.openFileDialog1_FileOk(sender: Object; e: CancelEventArgs);
+begin
+  if(listBox1.Items.Contains(openFileDialog1.FileName) = false) then listBox1.Items.Add(openFileDialog1.FileName);
+end;
+
+procedure Form1.button2_Click(sender: Object; e: EventArgs);
+begin
+  listBox1.Items.Remove(listBox1.SelectedItem);
+  if(listBox1.SelectedItem = nil) then begin
+    button3.Enabled := false;
+    playSelectedSoundToolStripMenuItem.Enabled := false;
+    button2.Enabled := false;
+    removeToolStripMenuItem.Enabled := false;
+  end;
+end;
+
+procedure Form1.playSelectedSoundToolStripMenuItem_Click(sender: Object; e: EventArgs);
+begin
+  var SoundboardPlayer := new System.Windows.Media.MediaPlayer;
+  SoundboardPlayer.Open(new System.Uri(listBox1.SelectedItem.ToString, System.UriKind.Relative));
+  SoundboardPlayer.Play();
+  sleep((maskedTextBox1.Text + '000').ToInteger);
+  SoundboardPlayer.Close();
+end;
+
+procedure Form1.addAFileToolStripMenuItem_Click(sender: Object; e: EventArgs);
+begin
+  openFileDialog1.ShowDialog;
+end;
+
+procedure Form1.removeToolStripMenuItem_Click(sender: Object; e: EventArgs);
+begin
+  listBox1.Items.Remove(listBox1.SelectedItem);
+  if(listBox1.SelectedItem = nil) then begin
+    button3.Enabled := false;
+    playSelectedSoundToolStripMenuItem.Enabled := false;
+    button2.Enabled := false;
+    removeToolStripMenuItem.Enabled := false;
+  end;
+end;
+
+end.
